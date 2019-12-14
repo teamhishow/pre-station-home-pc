1776f6059dcecdd37f85f94545462e6e24294614
Yu Nakata <y-nakata@outlook.com>
2019-12-14 13:55:35 +0900

    まずは、スキャンしたビーコンを、httpで次の駅のPCに送信する

--- beacon.js
+++ beacon.js
@@ -1,14 +1,28 @@
-Bleacon = require('bleacon');
+const request = require('request');
+const fs = require('fs');
+const Bleacon = require('bleacon');
+
 const uuid = 'aaaaaaaabbbbccccddddeeeeeeeeeeee';
-//const uuid = 'AAAAAAAABBBBCCCCDDDDEEEEEEEEEEEE';
-//const uuid = '222B4D44-A86A-4A44-B270-AB04311924F6';
 const major = 12;
 const minor = 34;
 const measuredPower = -59;
 
+let http_address = fs.readFileSync("/tmp/hishow/http/next-station", 'utf-8');
+http_address = http_address.replace(/\r?\n/g, '');
+
 Bleacon.startScanning(uuid);
+
 Bleacon.on('discover', function(bleacon) {
-   console.log(bleacon);
+    var options = {
+        url: http_address,
+        method: "post",
+        form: {"name":"太郎"}
+    }
+    request(options, function (error, response, body) {
+        console.log(body);
+    })
+
 });
+
 Bleacon.startAdvertising(uuid, major, minor, measuredPower);
 
