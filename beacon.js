12c3f9ed4d132bb7a0bd2cc458a5511685a3bfb6
Yu Nakata <y-nakata@outlook.com>
2019-12-15 08:10:04 +0900

    追加情報

--- beacon.js
+++ beacon.js
@@ -27,6 +27,11 @@ Bleacon.on('discover', function(bleacon) {
     } else {
         congestions[bleacon.major] = bleacon.minor;
     }
+
+    var train_id = Number(bleacon.major.slice(0,3));
+    var car_id = Number(bleacon.major.slice(-2));
+    var door_id = Number(bleacon.major.slice(-1));
+
     var dt = new Date();
     var formatted_date = dt.toFormat("YYYY/MM/DD/HH24/MI/SS");
     request.post({
@@ -34,7 +39,14 @@ Bleacon.on('discover', function(bleacon) {
         headers: {
             "content-type": "application/json"
         },
-        body: JSON.stringify({door_id: bleacon.major, congestion: bleacon.minor, time: formatted_date})
+        body: JSON.stringify({
+            station: "神田",
+            train_id: train_id,
+            car_id: car_id,
+            door_id: door_id,
+            congestion: bleacon.minor,
+            time: formatted_date
+        })
     }, function (error, response, body){
         console.log(body);
     });
